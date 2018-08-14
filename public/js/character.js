17362922790a391b03eb13da794c952c3ce73144
David <d.vinternatt@gmail.com>
2018-08-14 03:19:19 +0100

    refactoring: looping through keys

--- public/js/character.js
+++ public/js/character.js
@@ -5,55 +5,29 @@
 const statValue = e => document.getElementById(e).value
 
 const updateLocalCharacter = () => {
-    localCharacter.bodyAttr.body = Number(statValue("body"))
-    localCharacter.bodyAttr.athletics = Number(statValue("athletics"))
-    localCharacter.bodyAttr.brawl = Number(statValue("brawl"))
-    localCharacter.bodyAttr.force = Number(statValue("force"))
-    localCharacter.bodyAttr.melee = Number(statValue("melee"))
-    localCharacter.bodyAttr.stamina = Number(statValue("stamina"))
-    localCharacter.bodyAttr.toughness = Number(statValue("toughness"))
+    for (let key in localCharacter["bodyAttr"]) {
+        localCharacter["bodyAttr"][key] = Number(statValue(key))
+    }
 
-    localCharacter.agilityAttr.agility = Number(statValue("agility"))
-    localCharacter.agilityAttr.crafting = Number(statValue("crafting"))
-    localCharacter.agilityAttr.dexterity = Number(statValue("dexterity"))
-    localCharacter.agilityAttr.navigation = Number(statValue("navigation"))
-    localCharacter.agilityAttr.mobility = Number(statValue("mobility"))
-    localCharacter.agilityAttr.projectiles = Number(statValue("projectiles"))
-    localCharacter.agilityAttr.stealth = Number(statValue("stealth"))
+    for (let key in localCharacter["agilityAttr"]) {
+        localCharacter["agilityAttr"][key] = Number(statValue(key))
+    }
 
-    localCharacter.charismaAttr.charisma = Number(statValue("charisma"))
-    localCharacter.charismaAttr.arts = Number(statValue("arts"))
-    localCharacter.charismaAttr.conduct = Number(statValue("conduct"))
-    localCharacter.charismaAttr.expression = Number(statValue("expression"))
-    localCharacter.charismaAttr.leadership = Number(statValue("leadership"))
-    localCharacter.charismaAttr.negotiation = Number(statValue("negotiation"))
-    localCharacter.charismaAttr.seduction = Number(statValue("seduction"))
+    for (let key in localCharacter["charismaAttr"]) {
+        localCharacter["charismaAttr"][key] = Number(statValue(key))
+    }
 
-    localCharacter.intellectAttr.intellect = Number(statValue("intellect"))
-    localCharacter.intellectAttr.artifactLore = Number(
-        statValue("artifactLore")
-    )
-    localCharacter.intellectAttr.engineering = Number(statValue("engineering"))
-    localCharacter.intellectAttr.focus = Number(statValue("focus"))
-    localCharacter.intellectAttr.legends = Number(statValue("legends"))
-    localCharacter.intellectAttr.medicine = Number(statValue("medicine"))
-    localCharacter.intellectAttr.science = Number(statValue("science"))
+    for (let key in localCharacter["intellectAttr"]) {
+        localCharacter["intellectAttr"][key] = Number(statValue(key))
+    }
 
-    localCharacter.psycheAttr.psyche = Number(statValue("psyche"))
-    localCharacter.psycheAttr.cunning = Number(statValue("cunning"))
-    localCharacter.psycheAttr.deception = Number(statValue("deception"))
-    localCharacter.psycheAttr.domination = Number(statValue("domination"))
-    localCharacter.psycheAttr.faith = Number(statValue("faith"))
-    localCharacter.psycheAttr.reaction = Number(statValue("reaction"))
-    localCharacter.psycheAttr.willpower = Number(statValue("willpower"))
+    for (let key in localCharacter["psycheAttr"]) {
+        localCharacter["psycheAttr"][key] = Number(statValue(key))
+    }
 
-    localCharacter.instinctAttr.instinct = Number(statValue("instinct"))
-    localCharacter.instinctAttr.empathy = Number(statValue("empathy"))
-    localCharacter.instinctAttr.orienteering = Number(statValue("orienteering"))
-    localCharacter.instinctAttr.perception = Number(statValue("perception"))
-    localCharacter.instinctAttr.primal = Number(statValue("primal"))
-    localCharacter.instinctAttr.survival = Number(statValue("survival"))
-    localCharacter.instinctAttr.taming = Number(statValue("taming"))
+    for (let key in localCharacter["instinctAttr"]) {
+        localCharacter["instinctAttr"][key] = Number(statValue(key))
+    }
 
     document.getElementById("ego").value = localCharacter.egoValue()
     document.getElementById(
